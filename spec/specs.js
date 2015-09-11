98e54e9b330ac1f28df4b81afbcd2138dbec2354
Kellen Kolbeck <kellengkolbeck@gmail.com>
2015-09-11 11:28:17 -0700

    Add spec to find and replace a word

---
+++ spec/specs.js
@@ -0,0 +1,13 @@
+describe("wordReplacement", function() {
+  it("allows a user to input a string", function() {
+    expect("This is a house").to.eql("This is a house")
+  });
+
+  it("allows a user to enter a find word and a replace word", function() {
+    expect("This is a house", "house", "bed").to.eql("This is a house", "house", "bed")
+  });
+
+  it("replaces the found word with a new word of user's choice", function() {
+    expect(wordReplacement("This is a house", "house", "bed")).to.eql("This is a bed")
+  });
+});
